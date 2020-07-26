8598dc587f0b0f9bbedb2a6f02e3155cb2a86d00
Luke O'Malley <Lukeomalley95@gmail.com>
2020-07-26 17:32:41 -0400

    setup bolt db connection and task list functionality

--- cmd/list.go
+++ cmd/list.go
@@ -2,16 +2,33 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/lukeomalley/task/db"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
-	Use:   "list",
+	Use:   "ls",
 	Short: "List all of your tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong: ", err.Error())
+			os.Exit(1)
+		}
+
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks left! 🤘🏻")
+			return
+		}
+
+		fmt.Println("You have the following tasks. Get on it bud. 🏃‍♂️")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s", i+1, task.Value)
+		}
+
 	},
 }
 
